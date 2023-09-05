68c7d0514ece109deb930adc995c140bdc0416c2
Ghada-alshehri <143977601+Ghada-alshehri@users.noreply.github.com>
2023-09-05 19:03:02 +0300

    Update Cpit251_.java

--- src/cpit251_/Cpit251_.java
+++ src/cpit251_/Cpit251_.java
@@ -15,7 +15,7 @@ public class Cpit251_ {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.err.println("hello world ");    }
     }
     
 }
